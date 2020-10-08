54345b9428f405e87ad0f946d9088673c4b3af93
Jonathon Lau <jonathon_lau@hotmail.com>
2020-10-07 18:04:05 -0600

    Global Manager Stuffs

---
+++ Assets/Scripts/GlobalManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalManager : Singleton<GlobalManager>
+{
+    public GameObject player = null;
+
+    public OptionsManager optionsManager = null;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
