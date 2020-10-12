3978fdb319cf14801c8ce3d063dae8a35440141a
Yoannyz <38017282+Yoannyz@users.noreply.github.com>
2020-10-12 19:10:49 -0400

    Level Management
    Level management is in. Its linear so we will need to take a look back if we want to make it better for the future.

---
+++ Assets/Scripts/Managers/Level Management Scripts/LevelComplete.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelComplete : MonoBehaviour
+{
+    int LevelAmount = 50;
+    private int currentLevel;
+
+    private void Start()
+    {
+        CheckLevel();
+    }
+
+    void CheckLevel()
+    {
+        for (int i = 1; i <= LevelAmount; i++)
+        {
+            if (SceneManager.GetActiveScene().name == "Level" + i)
+            {
+                currentLevel = i;
+                SaveMyGame();
+            }
+        }
+    }
+
+    void SaveMyGame()
+    {
+        int NextLevel = currentLevel + 1;
+        if (NextLevel <= LevelAmount)
+        {
+            PlayerPrefs.SetInt("Level" + NextLevel.ToString(), 1);
+        }
+
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/Managers/Level Management Scripts/LevelLockManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLockManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Level
+    {
+
+        public string LevelText;
+        public int Unlock;
+        public bool isInteractible;
+
+        public Button.ButtonClickedEvent OnClick;
+    }
+
+    public Button[] levelButtons;
+    public GameObject LEVELButton;
+    public Transform Spacer;
+    public List<Level> LevelList;
+
+    private void Start()
+    {
+        
+    }
+
+    void FillOutList()
+    {
+        LevelButton prevButton = null;
+        for (int i = 0; i <= LevelList.Count; i++)
+        {
+            GameObject newbutton = Instantiate(LEVELButton) as GameObject;
+            LevelButton button = newbutton.GetComponent<LevelButton>();
+
+
+            button.LevelText.text = LevelList[i].LevelText;
+            Debug.Log(button.LevelText.text);
+
+            if (i > 0)
+            {
+
+                if (PlayerPrefs.GetInt("Level" + prevButton.LevelText.text + "timetrial") == 1)
+                {
+                    LevelList[i - 1].Unlock = 1;
+                    LevelList[i - 1].isInteractible = true;
+                }
+
+                if (PlayerPrefs.GetInt("Level" + prevButton.LevelText.text + "arena") == 1)
+                {
+                    LevelList[i - 1].Unlock = 1;
+                    LevelList[i - 1].isInteractible = true;
+                }
+
+            }
+
+            if (i < LevelList.Count)
+            {
+
+                if (PlayerPrefs.GetInt("Level" + button.LevelText.text) == 1)
+                {
+                    LevelList[i].Unlock = 1;
+                    LevelList[i].isInteractible = true;
+                }
+            }
+            button.unlocked = LevelList[i].Unlock;
+            button.GetComponent<Button>().interactable = LevelList[i].isInteractible;
+            button.GetComponent<Button>().onClick.AddListener(() => LoadLevel("Level" + button.LevelText.text));
+            //button.GetComponent<Button>().onClick.AddListener(() => StarCor("Level" + button.LevelText.text));
+
+
+
+            newbutton.transform.SetParent(Spacer);
+
+            prevButton = button;
+        }
+        SAVE();
+    }
+
+    void SAVE()
+    {
+        {
+            GameObject[] allbuttons = GameObject.FindGameObjectsWithTag("LevelButton");
+            foreach (GameObject buttons in allbuttons)
+            {
+                LevelButton button = buttons.GetComponent<LevelButton>();
+                PlayerPrefs.SetInt("Level" + button.LevelText.text, button.unlocked);
+            }
+        }
+    }
+
+    public void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    void LoadLevel(string value)
+    {
+        SceneManager.LoadScene(value);
+        //LoadingScene.SetActive(true);
+    }
+}
