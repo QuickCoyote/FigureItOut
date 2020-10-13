c9c470dcbc024e56099d7b845a8add81b97c262b
Yoannyz <38017282+Yoannyz@users.noreply.github.com>
2020-10-12 21:07:26 -0400

    Follow Enemy
    Started working on the follow enemy scripts

---
+++ Assets/Scripts/Enemy Scripts/Follow Enemies/FollowEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowEnemy : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.0f, 100.0f)]
+    private float speed = 10;
+
+    [SerializeField]
+    [Range(0.0f, 100.0f)]
+    private float damage = 10;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
