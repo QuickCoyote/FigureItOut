c2243a5d4c803b21930fe233a81617aa32d838b9
Yoannyz <38017282+Yoannyz@users.noreply.github.com>
2020-10-20 21:04:08 -0400

    Follow Enemy
    Need to keep the enemy from spinning after losing sight

--- Assets/Scripts/Enemy Scripts/Follow Enemies/FollowEnemy.cs
+++ Assets/Scripts/Enemy Scripts/Follow Enemies/FollowEnemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowEnemy : MonoBehaviour
+public class FollowEnemy : Enemy
 {
     [SerializeField]
     [Range(0.0f, 100.0f)]
@@ -12,14 +12,33 @@ public class FollowEnemy : MonoBehaviour
     [Range(0.0f, 100.0f)]
     private float damage = 10;
 
+    [SerializeField]
+    [Range(0.0f, 100.0f)]
+    private float drag = 10;
 
-    void Start()
+    private Vector3 direction = Vector3.zero;
+    private Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        if (CanSeeTarget())
+        {
+            rb.drag = 0.05f;
+            direction = target.transform.position - transform.position;
+            direction.Normalize();
+
+            Vector3 velocity = direction * speed;
+
+            rb.velocity = new Vector3(velocity.x, velocity.y, velocity.z);
+        }
+        else
+        {
+            rb.drag = (drag);
+        }
     }
 }
--- Assets/Scripts/Enemy Scripts/Follow Enemies/SpikeEnemy.cs
+++ Assets/Scripts/Enemy Scripts/Follow Enemies/SpikeEnemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpikeEnemy : MonoBehaviour
+public class SpikeEnemy : FollowEnemy
 {
     // Start is called before the first frame update
     void Start()
